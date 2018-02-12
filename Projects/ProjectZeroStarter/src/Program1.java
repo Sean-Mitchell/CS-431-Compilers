f5625817b9271e71871a9f7483fb5d5d6f7be622
Grant <gardng59@uwosh.edu>
2018-02-11 19:44:41 -0600

    added program1

---
+++ Projects/ProjectZeroStarter/src/Program1.java
@@ -0,0 +1,21 @@
+package Starter;
+
+public class Program{
+	//the syntax tree representation of:  echo(34)
+	//You should create separate programs for each tree you create.
+  	private static Stmts program = new ContinuingStmts(
+		new AssignStmt("one", new NumExp(30)),
+		new LastStmts(
+			new PrintStmt(new LastExpList(new IdExp("one")))
+		)
+	);
+
+	public static void main(String[] args) {
+		//Create a new Interpreter Object
+	    Interpreter interpreter = new Interpreter();
+	    System.out.println("Evaluating...");
+	    //Call the overloaded interpret method with the
+	    //static program created above. Should print out 34.
+	    interpreter.interpret(program);
+	}
+}
