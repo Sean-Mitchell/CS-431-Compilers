b7dc2d6d52096193ea25a6dc36de4a94e8ee2e58
Grant <gardng59@uwosh.edu>
2018-02-21 20:30:56 -0600

    Added a few methods to give structure

--- Projects/ProjectOne/scanner.java
+++ Projects/ProjectOne/scanner.java
@@ -1,5 +1,25 @@
 package Starter;
 
+import java.io.*;
+import java.util.*;
+
 public class Scanner {
+	public Token[] scanFile(String fileName) {
+		File f = new File(fileName);
+		FileReader fr = new FileReader(f);
+		
+		ArrayList<Token> tokenList = new ArrayList<>();
+		Token next = getNextToken(fr);
+		
+		while (next != null) {
+			tokenList.add(next);
+			next = getNextToken(fr);
+		}
+		
+		return tokenList;
+	}
 	
+	public Token getNextToken(FileReader fr) {
+		//TODO
+	}
 }
