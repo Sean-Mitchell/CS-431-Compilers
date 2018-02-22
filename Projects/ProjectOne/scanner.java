3fb751708d71ad7dd9c29519ac1af633fd58991b
Grant <gardng59@uwosh.edu>
2018-02-21 21:24:21 -0600

    in the middle of adding valid tokens

--- Projects/ProjectOne/scanner.java
+++ Projects/ProjectOne/scanner.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class Scanner {
+	private static ArrayList<Token> validTokens = new ArrayList<>();
+	
 	public Token[] scanFile(String fileName) {
 		File f = new File(fileName);
 		FileReader fr = new FileReader(f);
@@ -16,10 +18,47 @@ public class Scanner {
 			next = getNextToken(fr);
 		}
 		
-		return tokenList;
+		return tokenList; 
 	}
 	
-	public Token getNextToken(FileReader fr) {
+	private Token getNextToken(FileReader fr) {
 		//TODO
+	}
+	
+	private Token[] getPossibleTokens(String partialToken) {
+		
+	}
+	
+	private void initializeValidTokens() {
+		validTokens.add(new Token(0, "class"));
+		validTokens.add(new Token(1, "public"));
+		validTokens.add(new Token(2, "static"));
+		validTokens.add(new Token(3, "void"));
+		validTokens.add(new Token(4, "main"));
+		validTokens.add(new Token(5, "String"));
+		validTokens.add(new Token(6, "{"));
+		validTokens.add(new Token(7, "}"));
+		validTokens.add(new Token(8, "["));
+		validTokens.add(new Token(9, "]"));
+		validTokens.add(new Token(10, "("));
+		validTokens.add(new Token(11, ")"));
+		validTokens.add(new Token(12, "extends"));
+		validTokens.add(new Token(14, ";"));
+		validTokens.add(new Token(15, "return"));
+		validTokens.add(new Token(16, "int"));
+		validTokens.add(new Token(17, "boolean"));
+		validTokens.add(new Token(18, "if"));
+		validTokens.add(new Token(19, "else"));
+		validTokens.add(new Token(20, "while"));
+		validTokens.add(new Token(21, "System.out.println"));
+		validTokens.add(new Token(22, "="));
+		validTokens.add(new Token(23, "&&"));
+		validTokens.add(new Token(24, "<"));
+		validTokens.add(new Token(25, "+"));
+		validTokens.add(new Token(26, "-"));
+		validTokens.add(new Token(27, "*"));
+		validTokens.add(new Token(28, "."));
+		validTokens.add(new Token(29, "length"));
+		validTokens.add(new Token(30, ","));
 	}
 }
