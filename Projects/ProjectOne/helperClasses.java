773c504a0380c88ca094978bd952c454925b751c
Sean-Mitchell <seanmtchl@gmail.com>
2018-02-21 21:31:39 -0600

    Added Token
    Added Token to the token class so you can have the actual "<TOKEN>" associated with the string

--- Projects/ProjectOne/helperClasses.java
+++ Projects/ProjectOne/helperClasses.java
@@ -3,8 +3,10 @@ package Starter;
 class Token {
 	int type;
 	String value;
-	public Token(int type, String value) {
+	String token;
+	public Token(int type, String value, String token) {
 		this.type = type;
 		this.value = value;
+		this.token = token;
 	}
 }
