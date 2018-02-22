b445f656e28e192fafdfc5a2784ac118ab5881ee
Grant <gardng59@uwosh.edu>
2018-02-22 17:09:54 -0600

    Took token stuff out

--- Projects/ProjectOne/helperClasses.java
+++ Projects/ProjectOne/helperClasses.java
@@ -9,32 +9,4 @@ class Token {
 		this.value = value;
 		this.token = token;
 	}
-	
-	public Token(int type) {
-		this.type = type;
-		
-		switch (type) {
-			case 0:
-				value = "class";
-				token = "<TClass>";
-			case 1:
-				value = "public";
-				token = "<TPublic>";
-			case 2:
-				value = "static";
-				token = "<TStatic>";
-			case 3:
-				value = "void";
-				token = "<TVoid>";
-			case 4:
-				value = "main";
-				token = "<TMain>";
-			case 5:
-				value = "String";
-				token = "<TString>";
-			case 0:
-				value = "class";
-				token = "<TClass>";
-		}
-	}
 }
