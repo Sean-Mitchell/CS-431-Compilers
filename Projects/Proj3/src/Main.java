900529cfcf63ef9918592b4ddd4c1326e12fbfa2
Sean-Mitchell <seanmtchl@gmail.com>
2018-03-05 10:48:13 -0600

    Creation Of Project3
    Copied lots of project2 and put it into project3

---
+++ Projects/Proj3/src/Main.java
@@ -0,0 +1,23 @@
+package Project3;
+
+import Project3.lexer.*;
+import Project3.node.*;
+import java.io.*;
+
+public class Main{
+
+    public static void main(String[] arguments){
+            try{
+                // Create a lexer instance.
+                Lexer l = new Lexer(new PushbackReader
+                        (new InputStreamReader(System.in), 1024));
+
+                Token t = l.next();
+                while (!t.getText().equals("")){
+                        System.out.print("<"+t.toString()+">");
+                        t = l.next();
+                }
+            }
+            catch(Exception e){ System.out.println(e.getMessage()); }
+    }
+}
