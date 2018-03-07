26d1169245804c98cce96896a2a4ba2e4f46fce0
Sean-Mitchell <seanmtchl@gmail.com>
2018-03-06 22:21:42 -0600

    Updated Main and expression
    Made expression more complex, made it so main handles more stuff

--- Projects/ProjectThree/src/Main.java
+++ Projects/ProjectThree/src/Main.java
@@ -29,49 +29,63 @@ public class Main{
         catch(Exception e){ System.out.println(e.getMessage()); }
     }
     
+    //Initializes the statement and uses a recursive end to create a list of statements
     private static String RecursiveParse() {
-    	ParseString.append("new Stmts(" + GetStatement());
+    	String returnString = "new Stmts(" + GetStatement();
     	
     	//Keep going, there are other statements to parse
+    	currentToken = GetNextToken();
     	if (currentToken.getClass().getSimpleName().equals("TSemicolon")) {
-    		ParseString.append(", " + RecursiveParse());
+    		returnString += ", " + RecursiveParse();
     	}
     	
-    	return ParseString.append(")").toString();
+    	returnString += ")";
+    	return returnString;
     }
     
+    //Get's the type of statement
     private static String GetStatement() {
+    	String returnString;
     	//Gets the first token
     	currentToken = GetNextToken();
     	if (currentToken.getClass().getSimpleName().equals("TId")) {
     		//Reads left paren
         	currentToken = GetNextToken();
-    		return "new AssignStmt(" + GetExpression() + ")";
+        	returnString = "new AssignStmt(" + GetExpression() + ")";
+    		//Reads right paren
+        	currentToken = GetNextToken();
+    		return returnString;
     	}
     	else if (currentToken.getClass().getSimpleName().equals("TPrint")){
     		//Reads left paren
         	currentToken = GetNextToken();
-    		return "new PrintStmt(" + GetExpression() + ")";
+        	returnString = "new PrintStmt(" + GetExpression() + ")";
+    		//Reads right paren
+        	currentToken = GetNextToken();
+    		return returnString;
     	}
     	return"";
     }
     
+    //Gets the type of expression
     private static String GetExpression() {
     	currentToken = GetNextToken();
     	System.out.println(currentToken.getClass());
     	if (currentToken.getClass().getSimpleName().equals("TNumber")) {
     		return "new NumExp(" + currentToken.getText()  + ")";
     	}
-    	else if (currentToken.getClass().getSimpleName().equals("TPrint")){
-    		return "new PrintStmt(" + GetExpression() + ")";
+    	else if (currentToken.getClass().getSimpleName().equals("TId")){
+    		return "new IdExp(" + currentToken.getText() + ")";
     	}
     	return "";
     }
     
+    //Get's next token
     private static Token GetNextToken() {
     	Token nextToken = null;
     	try{
     		nextToken = theLexer.next();
+    		//Gets tokens until it's not blank
 	    	while (nextToken.getClass().getSimpleName().equals("TBlank")) {
 	    		nextToken = theLexer.next();
 	    	}
