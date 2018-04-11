d4d3a2640699a3a694cdc788d74bb94fc3c434ba
Grant <gardng59@uwosh.edu>
2018-04-10 20:15:52 -0500

    break time

--- Projects/Project4/src/ProjFour.js
+++ Projects/Project4/src/ProjFour.js
@@ -5,14 +5,14 @@ Helpers
     number = ['0'..'9'];
     letter = ['a'..'z'] | ['A'..'Z'];
     anychar = [35..255];
-	dot = '.';
+	
 
 	add = '+';
 	subtract = '-';
 	times = '*';
 	divide = '/';
 
-	quote = '"';
+	
 	lparenh = '(';
 	rparenh = ')';	
 	lbracket = '[';
@@ -35,6 +35,32 @@ Tokens
 	end = 'END';
 	true = 'TRUE';
 	false = 'FALSE';
+	class = 'CLASS';
+	lcurly = '{';
+	rcurly = '}';
+	comma = ',';
+	colon = ':';
+	lbrack = '[';
+	rbrack = ']';
+	equal = '=';
+	semicolon = ';';
+	quote = '"';
+	if = 'IF';
+	then = 'THEN';
+	else = 'ELSE';
+	while = 'WHILE';
+	for = 'FOR';
+	plus = '+';
+	minus = '-';
+	put = 'PUT';
+	get = 'GET';
+	new = 'NEW';
+	return = 'RETURN';
+	dot = '.';
+	switch = 'SWITCH';
+	case = 'CASE';
+	break = 'BREAK';
+	default = 'DEFAULT';
 	
 	equivalent = '==';
 	notequivalent = '!=';
@@ -47,20 +73,36 @@ Ignored Tokens
   whitespace;
 
 Productions
-    prog = {first} id digittok |
-    	   {second} lotnumbers |
-    	   {third} [eachsymbolisuniqueinaproduction]:id [secondid]:id [digitone]:digittok [digittwo]:digittok ;
-    lotnumbers = digittok morenumbers;
-    morenumbers = {fourth} digittok morenumbers |
-    		  {emptyproduction} ;
-
-	factor = {first} lparen exp rparen |
-				{second} subtract factor |
-				{third} int | real |
-				{fourth} boolean ;
-				
+    prog = begin classmethodstmts end;
 	
-
+	classmethodstmts = {first} classmethodstmts classmethodstmt |
+		{empty} ;
+		
+	classmethodstmt = {first} class id lcurly methodstmtseqs rcurly |
+		{second} type id lparen varlist rparen lcurly stmtseq rcurly |
+		{third} id (comma id)* colon type semicolon ;
+		
+	methodstmtseqs = {first} methodstmtseqs methodstmtseq |
+		{empty} ;
+		
+	methodstmtseq = {first} type id lparen varlist rparen lcurly stmtseq rcurly |
+		{second} id (comma id)* colon type semicolon ;
+		
+	stmtseq = {first} stmt stmtseq |
+		{empty} ;
+		
+	stmt = {third} id (comma id)* colon type (lbrack int rbrack)? semicolon |
+		{fourth} if lparen boolean rparen then lcurly stmtseq rcurly (else lcurly stmtseq rcurly)? |
+		{fifth} while lparen boolean rparen lcurly stmtseq rcurly |
+		{sixth} for lparen (type)? id colon equal expr semicolon boolean semicolon (id plus plus | id minus minus | id colon equal expr) rparen lcurly stmtseq rcurly |
+		{seventh} id ( lbrack int rbrack )? colon equal get lparen rparen semicolon |
+		{eigth} put lparen id (lbrack int rbrack)? rparen semicolon |
+		{ninth} id (lbrack int rbrack)? (plus plus semicolon | minus minus semicolon | colon equal new id lparen rparen semicolon | colon equal (expr semicolon | quote anychars quote semicolon | boolean semicolon)) |
+		{tenth} id lparen varlisttwo rparen semicolon |
+		{eleventh} id (lbrack int rbrack)? dot id lparen varlisttwo rparen (dot id lparen varlisttwo rparen )* semicolon |
+		{twelfth} return expr semicolon |
+		{thirteenth} 
+		
 	string = string;
 	
 	boolean = {first} true | false |
