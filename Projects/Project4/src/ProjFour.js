5e7b540f9b35809d9565643425b78ddd18b054f3
Sean-Mitchell <seanmtchl@gmail.com>
2018-04-10 22:30:17 -0500

    Updated spacing and tokens

--- Projects/Project4/src/ProjFour.js
+++ Projects/Project4/src/ProjFour.js
@@ -17,20 +17,8 @@ Helpers
 	rparenh = ')';	
 	lbracket = '[';
 	rbracket = ']';
+	dot = '.';
 
-
-    alphanumeric = letter | number;
-    
-Tokens
-	lparen = lparenh;
-	rparen = rparenh;
-    idtok = letter (letter | number)*;
-    digittok = number+;
-	digsing = number;
-	real = number+ dot number+;
-	stringtok = quote (number | letter)* quote;
-    whitespace = sp+;
-	anychars = anychar+;
 	begin = 'BEGIN';
 	end = 'END';
 	true = 'TRUE';
@@ -56,7 +44,6 @@ Tokens
 	get = 'GET';
 	new = 'NEW';
 	return = 'RETURN';
-	dot = '.';
 	switch = 'SWITCH';
 	case = 'CASE';
 	break = 'BREAK';
@@ -69,6 +56,19 @@ Tokens
 	less = '<';
 	greater = '>';
 
+    alphanumeric = letter | number;
+    
+Tokens
+	lparen = lparenh;
+	rparen = rparenh;
+    idtok = letter (letter | number)*;
+    digittok = number+;
+	digsing = number;
+	real = number+ dot number+;
+	stringtok = quote (number | letter)* quote;
+    whitespace = sp+;
+	anychars = anychar+;
+
 Ignored Tokens
   whitespace;
 
@@ -76,7 +76,7 @@ Productions
     prog = begin classmethodstmts end;
 	
 	classmethodstmts = {first} classmethodstmts classmethodstmt |
-		{empty} ;
+		{empty};
 		
 	classmethodstmt = {first} class id lcurly methodstmtseqs rcurly |
 		{second} type id lparen varlist rparen lcurly stmtseq rcurly |
@@ -121,26 +121,26 @@ Productions
 	
 	boolean = {first} true | {fourth}false |
 		{second} [first]:expr cond [second]:expr | 
-		{third} id;
+		{third} id ;
 
 	cond = {first} equivalent |
 		{second} notequivalent |
 		{third} geq | {fourth} leq |
-		{fifth} greater | {sixth} less;
+		{fifth} greater | {sixth} less ;
 
 	addop = {first} add | {second} subtract;
-	multop = {first} times | {second} divide;
+	multop = {first} times | {second} divide ;
 
 	type = {first} int | 
 		{second} real |
 		{third} string |
 		{fourth} boolean |
 		{fifth} void | 
-		{sixth} id;
+		{sixth} id ; 
 			
-	id = idtok;
-	int = digittok;
-	real = real;
-	anychars = anychars;
-	letter = letter;
-	digit = digsing;
+	id = idtok ;
+	int = digittok ;
+	real = real ;
+	anychars = anychars ;
+	letter = letter ;
+	digit = digsing ;
