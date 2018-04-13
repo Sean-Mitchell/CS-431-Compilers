e74b1b808d1f1fc28d967f3b09ff43e6238c21d6
Sean-Mitchell <seanmtchl@gmail.com>
2018-04-12 19:00:32 -0500

    Updated from the fugged branch
    Tried to do a merge so master is in sync with grants changes

--- Projects/Project4/src/ProjFour.js
+++ Projects/Project4/src/ProjFour.js
@@ -17,7 +17,6 @@ Helpers
 	rparenh = ')';	
 	lbracket = '[';
 	rbracket = ']';
-	dot = '.';
 
 	begin = 'BEGIN';
 	end = 'END';
@@ -56,19 +55,6 @@ Helpers
 	less = '<';
 	greater = '>';
 
-    alphanumeric = letter | number;
-    
-Tokens
-	lparen = lparenh;
-	rparen = rparenh;
-    idtok = letter (letter | number)*;
-    digittok = number+;
-	digsing = number;
-	real = number+ dot number+;
-	stringtok = quote (number | letter)* quote;
-    whitespace = sp+;
-	anychars = anychar+;
-
 Ignored Tokens
   whitespace;
 
@@ -76,36 +62,23 @@ Productions
     prog = begin classmethodstmts end;
 	
 	classmethodstmts = {first} classmethodstmts classmethodstmt |
-		{empty};
 		
 	classmethodstmt = {first} class id lcurly methodstmtseqs rcurly |
 		{second} type id lparen varlist rparen lcurly stmtseq rcurly |
-		{third} id commaid* colon type semicolon ;
 		
 	methodstmtseqs = {first} methodstmtseqs methodstmtseq |
 		{empty} ;
 		
 	methodstmtseq = {first} type id lparen varlist rparen lcurly stmtseq rcurly |
-		{second} id commaid* colon type semicolon ;
 		
 	stmtseq = {first} stmt stmtseq |
 		{empty} ;
 		
-	stmt = {third} id commaid* colon type (lbrack int rbrack)? semicolon |
-		{fourth} if lparen boolean rparen then [first]:lcurly [first]:stmtseq [first]:rcurly (else [second]:lcurly [second]:stmtseq [second]:rcurly)? |
 		{fifth} while lparen boolean rparen lcurly stmtseq rcurly |
-		{sixth} for lparen (type)? id [first]:colon [first]:equal [first]:expr [first]:semicolon boolean [second]:semicolon ([first]:id [first]:plus [second]:plus | [second]:id [first]:minus [second]:minus | [third]:id [second]:colon [second]:equal [second]:expr) rparen lcurly stmtseq rcurly |
-		{seventh} id ( lbrack int rbrack )? colon equal get lparen rparen semicolon |
-		{eigth} put lparen id (lbrack int rbrack)? rparen semicolon |
-		{ninth} [first]:id (lbrack int rbrack)? ([first]:plus [second]:plus [first]:semicolon | [first]:minus [second]:minus [second]:semicolon | [first]:colon [first]:equal new [second]:id lparen rparen [third]:semicolon | [second]:colon [second]:equal (expr [fourth]:semicolon | [first]:quote anychars [second]:quote [fifth]:semicolon | boolean [sixth]:semicolon)) |
 		{tenth} id lparen varlisttwo rparen semicolon |
-		{eleventh} [first]:id (lbrack int rbrack)? [first]:dot [second]:id [first]:lparen [first]:varlisttwo [first]:rparen ([second]:dot [third]:id [second]:lparen [second]:varlisttwo [second]:rparen )* semicolon |
 		{twelfth} return expr semicolon |
-		{thirteenth} switch [first]:lparen expr [first]:rparen lcurly [first]:case [second]:lparen [first]:int [second]:rparen [first]:colon [first]:stmtseq ([first]:break [first]:semicolon)? ([second]:case [third]:lparen [second]:int [third]:rparen [second]:colon [second]:stmtseq ([second]:break [second]:semicolon)?)* default [third]:colon [third]:stmtseq rcurly ;
 		
-	varlist = ([first]:id [first]:colon [first]:type ([first]:lbrack [first]:int [first]:rbrack)? (comma [second]:id [second]:colon [second]:type ([second]:lbrack [second]:int [second]:rbrack)?)*)? ;
 	
-	varlisttwo = ([first]:expr (comma [second]:expr)*)? ;
 	
 	expr = {first} expr addop term |
 		{second} term ;
@@ -115,35 +88,20 @@ Productions
 		{third} int |
 		{fourth} real |
 		{fifth} boolean |
-		{sixth} [first]:id (((lbrack int rbrack)? (dot [second]:id [first]:lparen [first]:varlisttwo [first]:rparen)?) | ([second]:lparen [second]:varlisttwo [second]:rparen)) ;
 		
 	string = stringtok;
 	
 	boolean = {first} true | {fourth}false |
 		{second} [first]:expr cond [second]:expr | 
-		{third} id ;
 
 	cond = {first} equivalent |
 		{second} notequivalent |
 		{third} geq | {fourth} leq |
-		{fifth} greater | {sixth} less ;
 
 	addop = {first} add | {second} subtract;
-	multop = {first} times | {second} divide ;
 
 	type = {first} int | 
 		{second} real |
 		{third} string |
 		{fourth} boolean |
 		{fifth} void | 
-		{sixth} id ; 
-	
-
-	
-	id = idtok ;
-	int = digittok ;
-	real = real ;
-	anychars = anychars ;
-	letter = letter ;
-	digit = digsing ;
-	commaid = comma id ;
