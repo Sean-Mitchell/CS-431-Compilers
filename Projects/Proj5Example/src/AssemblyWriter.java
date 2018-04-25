41c682030a86c46e0669682be998296ca3b6405c
Grant <gardng59@uwosh.edu>
2018-04-24 22:04:33 -0500

    started assembly writer file

---
+++ Projects/Proj5Example/src/AssemblyWriter.java
@@ -0,0 +1,358 @@
+package Project5;
+
+import Project5.analysis.*;
+import Project5.node.*;
+import java.util.*;
+
+class AssemblyWriter extends DepthFirstAdapter
+{	
+	SymbolTable symbolTable;
+	Hashtable<String, Integer> varAddressOffsets;
+
+ 	public AssemblyWriter(SymbolTable symbolTable) {
+		System.out.println("Creating assembly code");
+		this.symbolTable = symbolTable;
+		varAddressOffsets = new Hashtable<>();
+		
+		
+	}
+	
+	public void caseAProg(AProg node) {
+        node.getClassmethodstmts().apply(this);
+	}
+	
+	public void caseAClasStmtsClassmethodstmts(AClassStmtsClassmethodstmts node) {
+		node.getClassmethodstmts().apply(this);
+		node.getClassmethodstmt().apply(this);
+	}
+	
+	public void caseAEpsilonClassmethodstmts(AEpsilonClassmethodstmts node) {
+		
+	}
+	
+	public void caseAClassDefClassmethodstmt(AClassDefClassmethodstmt node) {
+		node.getMethodstmtseqs().apply(this);
+	}
+	
+	public void caseAMethodDeclClassmethodstmt(AMethodDeclClassmethodstmt node) {
+
+	}
+	
+	public void caseAVarDeclClassmethodstmt(AVarDeclClassmethodstmt node) {
+
+	}
+	
+	public void caseAMethodStmtsMethodstmtseqs(AMethodStmtsMethodstmtseqs node) {
+		node.getMethodstmtseqs().apply(this);
+		node.getMethodstmtseq().apply(this);
+	}
+	
+	public void caseAEpsilonMethodstmtseqs(AEpsilonMethodstmtseqs node) {
+		
+	}
+	
+	public void caseAMethodDeclMethodstmtseq(AMethodDeclMethodstmtseq node) {
+
+	}
+	
+	public void caseAVarDeclMethodstmtseq(AVarDeclMethodstmtseq node) {
+
+	}
+	
+	public void caseAAssignEqualsMethodstmtseq(AAssignEqualsMethodstmtseq node) {
+		node.getExpr().apply(this);
+	}
+	
+	public void caseAAssignStringMethodstmtseq(AAssignStringMethodstmtseq node) {
+		
+	}
+	
+	public void caseAPrintStmtMethodstmtseq(APrintStmtMethodstmtseq node) {
+		
+	}
+	
+	public void caseAAssignReadInMethodstmtseq(AAssignReadInMethodstmtseq node) {
+		
+	}
+	
+	public void caseAAssignIncMethodstmtseq(AAssignIncMethodstmtseq node) {
+		
+	}
+	
+	public void caseAAssignDecMethodstmtseq(AAssignDecMethodstmtseq node) {
+		
+	}
+	
+	public void caseADeclObjectMethodstmtseq(ADeclObjectMethodstmtseq node) {
+
+	}
+	
+	public void caseAAssignBooleanMethodstmtseq(AAssignBooleanMethodstmtseq node) {
+		
+	}
+	
+	public void caseAFirstStmtStmtseq(AFirstStmtStmtseq node) {
+		node.getStmt().apply(this);
+		node.getStmtseq().apply(this);
+	}
+	
+	public void caseAEpsilonStmtseq(AEpsilonStmtseq node) {
+		
+	}
+	
+	public void caseAAssignExprStmt(AAssignExprStmt node) {
+		
+	}
+	
+	public void caseAAssignStringStmt(AAssignStringStmt node) {
+		
+	}
+	
+	public void caseAVarDeclStmt(AVarDeclStmt node) {
+
+	}
+	
+	public void caseAIfBlockStmt(AIfBlockStmt node) {
+		node.getStmtseq().apply(this);
+	}
+	
+	public void caseAIfElseBlockStmt(AIfElseBlockStmt node) {
+		node.getIfBlockStmts().apply(this);
+		node.getElseBlockStmts().apply(this);
+	}
+	
+	public void caseAWhileStmt(AWhileStmt node) {
+		node.getStmtseq().apply(this);
+	}
+	
+	public void caseAForStmt(AForStmt node) {
+
+	}
+	
+	public void caseAGetStmt(AGetStmt node) {
+		
+	}
+	
+	public void caseAPutStmt(APutStmt node) {
+		
+	}
+	
+	public void caseAIncrStmt(AIncrStmt node) {
+		
+	}
+	
+	public void caseADecrStmt(ADecrStmt node) {
+		
+	}
+	
+	public void caseADeclObjectStmt(ADeclObjectStmt node) {
+
+	}
+	
+	public void caseAMethodCallStmt(AMethodCallStmt node) {
+		
+	}
+	
+	public void caseAMethodCallInClassStmt(AMethodCallInClassStmt node) {
+		
+	}
+	
+	public void caseAReturnStmt(AReturnStmt node) {
+		
+	}
+	
+	public void caseAAssignBooleanStmt(AAssignBooleanStmt node) {
+		
+	}
+	
+	public void caseASwitchStmt(ASwitchStmt node) {
+		node.getStmts().apply(this);
+		node.getDefaultStmts().apply(this);
+		node.getCaseHelper().apply(this);
+	}
+	
+	public void caseAAnotherCaseCaseHelper(AAnotherCaseCaseHelper node) {
+		node.getStmtseq().apply(this);
+		node.getCaseHelper().apply(this);
+	}
+	
+	public void caseAEpsilonCaseHelper(AEpsilonCaseHelper node) {
+		
+	}
+	
+	public void caseABreakBreakHelper(ABreakBreakHelper node) {
+		
+	}
+	
+	public void caseAEpsilonBreakHelper(AEpsilonBreakHelper node) {
+		
+	}
+	
+	public void caseAMethodCallMethodChainingOption(AMethodCallMethodChainingOption node) {
+		
+	}
+	
+	public void caseAEpsilonMethodChainingOption(AEpsilonMethodChainingOption node) {
+		
+	}
+	
+	public void caseAIncrForIncrStep(AIncrForIncrStep node) {
+		
+	}
+	
+	public void caseADecrForIncrStep(ADecrForIncrStep node) {
+		
+	}
+	
+	public void caseAAssignmentForIncrStep(AAssignmentForIncrStep node) {
+		
+	}
+	
+	public void caseAForOptionalType(AForOptionalType node) {
+		
+	}
+	
+	public void caseAEpsilonForOptionalType(AEpsilonForOptionalType node) {
+		
+	}
+	
+	public void caseAMoreIdsMoreIds(AMoreIdsMoreIds node) {
+		
+	}
+	
+	public void caseAEpsilonMoreIds(AEpsilonMoreIds node) {
+		
+	}
+	
+	public void caseAMoreIdsVarlist(AMoreIdsVarlist node) {
+
+	}
+	
+	public void caseAEpsilonVarlist(AEpsilonVarlist node) {
+		
+	}
+	
+	public void caseAArrayArrayOption(AArrayArrayOption node) {
+		
+	}
+	
+	public void caseAEpsilonArrayOption(AEpsilonArrayOption node) {
+		
+	}
+	
+	public void caseAMoreIdsMoreVarlist(AMoreIdsMoreVarlist node) {
+		node.getVarlist().apply(this);
+	}
+	
+	public void caseAEpsilonMoreVarlist(AEpsilonMoreVarlist node) {
+		
+	}
+	
+	public void caseAVarListVarListTwo(AVarListVarListTwo node) {
+		
+	}
+	
+	public void caseAEpsilonVarListTwo(AEpsilonVarListTwo node) {
+		
+	}
+	
+	public void caseAExprOrBool(AExprOrBool node) {
+		
+	}
+	
+	public void caseABoolExprOrBool(ABoolExprOrBool node) {
+		
+	}
+	
+	public void caseAMoreVarListTwo(AMoreVarListTwo node) {
+		
+	}
+	
+	public void caseAEpsilonMoreVarListTwo(AEpsilonMoreVarListTwo node) {
+		
+	}
+	
+	public void caseAAddExpr(AAddExpr node) {
+		
+	}
+	
+	public void caseATermExpr(ATermExpr node) {
+		
+	}
+	
+	public void caseAMultTerm(AMultTerm node) {
+		
+	}
+	
+	public void caseAFactorTerm(AFactorTerm node) {
+		
+	}
+	
+	public void caseAExprFactor(AExprFactor node) {
+		
+	}
+	
+	public void caseAIntFactor(AIntFactor node) {
+		
+	}
+	
+	public void caseARealFactor(ARealFactor node) {
+		
+	}
+	
+	public void caseAArrayFactor(AArrayFactor node) {
+		
+	}
+	
+	public void caseAIdvarlistFactor(AIdvarlistFactor node) {
+		
+	}
+	
+	public void caseALastFactor(ALastFactor node) {
+		
+	}
+	
+	public void caseAArrayArrayOrId(AArrayArrayOrId node) {
+		
+	}
+	
+	public void caseAIdArrayOrId(AIdArrayOrId node) {
+		
+	}
+	
+	public void caseATrueBoolean(ATrueBoolean node) {
+		
+	}
+	
+	public void caseAFalseBoolean(AFalseBoolean node) {
+		
+	}
+	
+	public void caseAConditionalBoolean(AConditionalBoolean node) {
+		
+	}
+	
+	public void caseABooleanBoolid(ABooleanBoolid node) {
+		
+	}
+	
+	public void caseAIdBoolid(AIdBoolid node) {
+		
+	}
+	
+	public void caseAPlusAddop(APlusAddop node) {
+		
+	}
+	
+	public void caseAMinusAddop(AMinusAddop node) {
+		
+	}
+	
+	public void caseATypesType(ATypesType node) {
+		
+	}
+	
+	public void caseAIdType(AIdType node) {
+		
+	}
+}
